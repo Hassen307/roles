5cff14a7ea7800d62616cd5d1101d9bf60699cd2
Hassen <benabdelhafidh.hassen1@gmail.com>
2017-04-20 23:17:41 +0200

    migration changement

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -19,11 +19,12 @@ class UserTableSeeder extends Seeder
        
 
         $user = new User();
+        $user->role_id = 1;
         $user->name = 'Hassen ben abdelhafidh';
         $user->email = 'benabdelhafidh.hassen@gmail.com';
         $user->password = bcrypt('111111');
         $user->save();
-        $user->roles()->attach($role_admin);
+        
 
        
     }
