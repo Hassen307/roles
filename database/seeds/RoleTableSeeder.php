7f5d91c395adc465de9badae61c7b41081ef28d3
atefBB <atef.bettaib@gmail.com>
2017-04-23 15:03:04 +0100

    format code

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -6,25 +6,26 @@ use App\Role;
 
 class RoleTableSeeder extends Seeder
 {
-/**
-* Run the database seeds.
-*
-* @return void
-*/
-public function run()
-{
-	// create admin role this way
-	$admin = new Role();
-	$admin->name = 'admin';
-	$admin->display_name = 'User Administrator'; // optional
-	$admin->description = 'User is allowed to manage and edit other users'; // optional
-	$admin->save();
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// create admin role this way
+		$admin = new Role();
+		$admin->name = 'admin';
+		$admin->display_name = 'User Administrator'; // optional
+		$admin->description = 'User is allowed to manage and edit other users'; // optional
+		$admin->save();
 
-	$permissions = Permission::pluck('id');
+		$permissions = Permission::pluck('id');
 
-	// and assign all permission like as below
-	foreach ($permissions as $permission) {
-	$admin->attachPermission($permission);
+		// and assign all permission like as below
+		foreach ($permissions as $permission) 
+		{
+			$admin->attachPermission($permission);
+		}
 	}
-}
 }
